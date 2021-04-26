3845b6257ef0efe7f8893bae4276367f4026c1e2
jaeyong_park <jaeyong_park@tmax.co.kr>
2021-04-26 22:44:46 +0900

    feat(ui): Create styles for SignUpForm
    - Replace the styles from scss file to jss
    - Add MaxWidth

--- src/components/organisms/SignUpForm.jsx
+++ src/components/organisms/SignUpForm.jsx
@@ -2,10 +2,21 @@ import { makeStyles, Typography } from '@material-ui/core';
 import { BasicInfoCard, ExerciseCodeCard, SignUpFormButton } from 'components';
 import { MESSAGES } from 'data';
 import React, { useCallback, useState } from 'react';
-import 'scss/components/sign-up-form.scss';
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: '#fafafa',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    maxWidth: '48em',
+    overflowX: 'hidden',
+  },
   title: { margin: '1rem' },
+  form: { display: 'flex', flexDirection: 'column', padding: '0.5em 0' },
 }));
 
 export default React.memo(function SignUpForm(props) {
@@ -23,22 +34,24 @@ export default React.memo(function SignUpForm(props) {
   const classes = useStyles();
 
   return (
-    <article className="sign-up-form">
-      <Typography variant="h4" className={classes.title}>
-        {MESSAGES.signUp.title('존맥')}
-      </Typography>
-      <Typography variant="h6" className={classes.title}>
-        {MESSAGES.signUp.subtitle}
-      </Typography>
-      <form>
-        <BasicInfoCard value={value} updateValue={updateValue} />
-        <ExerciseCodeCard
-          name="exerciseCode"
-          value={value.exerciseCode}
-          updateValue={updateValue}
-        />
-      </form>
-      <SignUpFormButton value={value} />
-    </article>
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <Typography variant="h4" className={classes.title}>
+          {MESSAGES.signUp.title('존맥')}
+        </Typography>
+        <Typography variant="h6" className={classes.title}>
+          {MESSAGES.signUp.subtitle}
+        </Typography>
+        <form className={classes.form}>
+          <BasicInfoCard value={value} updateValue={updateValue} />
+          <ExerciseCodeCard
+            name="exerciseCode"
+            value={value.exerciseCode}
+            updateValue={updateValue}
+          />
+        </form>
+        <SignUpFormButton value={value} />
+      </div>
+    </div>
   );
 });
